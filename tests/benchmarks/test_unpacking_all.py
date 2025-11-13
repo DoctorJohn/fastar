21959f11cb7c25d8a98d8ddcc941e12950eda3c4
Jonathan Ehwald <github@ehwald.info>
2025-11-13 20:59:13 +0100

    Benchmark unpacking all without mtime

--- tests/benchmarks/test_unpacking_all.py
+++ tests/benchmarks/test_unpacking_all.py
@@ -20,3 +20,13 @@ def test_benchmark_unpacking_all_with_fastar(
             archive.unpack(target_path)
 
     benchmark(read_archive)
+
+
+def test_benchmark_unpacking_all_without_mtime_using_fastar(
+    benchmark, large_archive_path, read_mode, target_path
+):
+    def read_archive():
+        with fastar.open(large_archive_path, read_mode) as archive:
+            archive.unpack(target_path, preserve_mtime=False)
+
+    benchmark(read_archive)
